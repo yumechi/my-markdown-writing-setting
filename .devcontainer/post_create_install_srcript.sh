a5230a0f96a4fbf1ffeab249a2ee8593f99758a4
Motoki Hirao(yumechi) <yumechi0525@gmail.com>
2025-07-06 00:35:01 +0000

    change: asdf to mise

--- .devcontainer/post_create_install_srcript.sh
+++ .devcontainer/post_create_install_srcript.sh
@@ -1,4 +1,4 @@
-##!/bin/bash
+#!/bin/bash
 
 sudo apt update -y && sudo apt install -y gpg sudo wget curl
 sudo install -dm 755 /etc/apt/keyrings
@@ -6,3 +6,8 @@ wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/
 echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 sudo apt update
 sudo apt install -y mise
+
+mise trust
+mise install
+
+exit 0
